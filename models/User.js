aca558c7e669fef2c484991e0d0c4e957e65a6fe
unknown <prashanthmanohar.g@outlook.com>
2022-07-02 22:47:35 +0530

    Working on Optimizing UI

--- models/User.js
+++ models/User.js
@@ -51,40 +51,46 @@ User.prototype.login = function(){
 
 }
 
-User.prototype.validate = async function() {
-if (this.data.username=="") {this.errors.push("You must provide a username")}
-if(this.data.username != "" && !validator.isAlphanumeric(this.data.username)) {this.errors.push("Username can contain only numeric and numbers")}
-if (!validator.isEmail(this.data.email)) {this.errors.push("You must provide a valid email address")}
-if (this.data.password=="") {this.errors.push("You must provide a valid password")}
-if( this.data.password.length > 0  && this.data.password.length<12) {this.errors.push("Password must be atleast 12 characters")}
-if(this.data.password.length> 50) {this.errors.push("Password cannot exceed 50 characters")}
-if(this.data.username.length > 0  && this.data.username.length<3) {this.errors.push("username atleast 3 characters")}
-if(this.data.username.length> 30) {this.errors.push("Username cannot exceed 30 characters")}
-
-
-//Only if username is valid then check to see it is already exists
-
-if(this.data.username.length>2 && this.data.username.length<31 && validator.isAlphanumeric(this.data.username))
+User.prototype.validate = function()
 {
-   let usernameExists = await usersCollection.findOne({username: this.data.username})
-   if (usernameExists) {this.errors.push("that username is already taken")}
+   return new Promise(async (resolve, reject) => {
+    if (this.data.username=="") {this.errors.push("You must provide a username")}
+    if(this.data.username != "" && !validator.isAlphanumeric(this.data.username)) {this.errors.push("Username can contain only numeric and numbers")}
+    if (!validator.isEmail(this.data.email)) {this.errors.push("You must provide a valid email address")}
+    if (this.data.password=="") {this.errors.push("You must provide a valid password")}
+    if( this.data.password.length > 0  && this.data.password.length<12) {this.errors.push("Password must be atleast 12 characters")}
+    if(this.data.password.length> 50) {this.errors.push("Password cannot exceed 50 characters")}
+    if(this.data.username.length > 0  && this.data.username.length<3) {this.errors.push("username atleast 3 characters")}
+    if(this.data.username.length> 30) {this.errors.push("Username cannot exceed 30 characters")}
+    
+    
+    //Only if username is valid then check to see it is already exists
+    
+    if(this.data.username.length>2 && this.data.username.length<31 && validator.isAlphanumeric(this.data.username))
+    {
+       let usernameExists = await usersCollection.findOne({username: this.data.username})
+       if (usernameExists) {this.errors.push("that username is already taken")}
+    }
+    
+    //Only if email is valid then check to see it is already exists
+    
+    if(validator.isEmail(this.data.email))
+    {
+       let emailExists = await usersCollection.findOne({email: this.data.email})
+       if (emailExists) {this.errors.push("that email is already being taken")}
+    }
+
+    resolve()
+    }
+    )
+
 }
 
-//Only if email is valid then check to see it is already exists
-
-if(validator.isEmail(this.data.email))
-{
-   let emailExists = await usersCollection.findOne({email: this.data.email})
-   if (emailExists) {this.errors.push("that email is already being taken")}
-}
-}
-
-
-User.prototype.register = function() {
+User.prototype.register = async function() {
 
 //Step-1: Validate user data
 this.cleanUp()
-this.validate()
+await this.validate()
 
 //Step-2: Only if there are no valdiation errors then save the user data into a database
 
